bb967f63c2314d17b97039033d9b2608e43ad8e1
David Brinegar <david.brinegar@me.com>
2015-01-06 13:34:35 -0800

    just tag, no branch

--- Formula/netmon.rb
+++ Formula/netmon.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Netmon < Formula
   homepage 'https://github.com/dbrinegar/netmon'
-  url 'https://github.com/dbrinegar/netmon.git', :branch => '0.0.1' , :tag =>'0.0.1'
+  url 'https://github.com/dbrinegar/netmon.git', :tag =>'0.0.1'
 
   def install
     bin.install "testnet"
