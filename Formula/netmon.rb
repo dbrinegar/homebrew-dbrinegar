b13bea63e8eee2f857a6aad0803fc1066add5165
David Brinegar <david.brinegar@me.com>
2015-01-06 13:46:39 -0800

    v for version

--- Formula/netmon.rb
+++ Formula/netmon.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Netmon < Formula
   homepage 'https://github.com/dbrinegar/netmon'
-  url 'https://github.com/dbrinegar/netmon.git', :tag =>'0.0.1'
+  url 'https://github.com/dbrinegar/netmon.git', :tag =>'v0.0.1'
 
   def install
     bin.install "testnet"
