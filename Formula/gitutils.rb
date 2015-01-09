26c2683dbfcd2a249fbdac6ad41621fcf01e6a3e
David Brinegar <david.brinegar@me.com>
2015-01-09 13:41:21 -0800

    gitutils formula
    Simple script to install everything in bin/* to /usr/local/bin for brew
    install, and run everything in test/* for brew test.

---
+++ Formula/gitutils.rb
@@ -0,0 +1,18 @@
+require 'formula'
+
+class Gitutils < Formula
+  homepage 'https://github.com/dbrinegar/gitutils'
+  url 'https://github.com/dbrinegar/gitutils.git', :tag =>'v0.0.1'
+
+  def install
+    bin.install Dir["bin/*"]
+    (prefix+"test").install Dir["test/*"]
+  end
+
+  test do
+    Dir[prefix+"test/*"].each do |t|
+      system t
+      assert $?.success?
+    end
+  end
+end
